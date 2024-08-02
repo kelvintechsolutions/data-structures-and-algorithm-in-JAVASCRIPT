a86c8afbc46915bae08bc5eaaee483d8e7692d6e
kelvintechsolutions <kevohmutugi51@gmail.com>
2024-08-02 10:11:51 +0300

    updates

--- strore back and front.js
+++
@@ -1,7 +0,0 @@
-const words = ["apple", "banana", "cherry", "date", "elderberry"];
-
-// Display forward
-console.log("Forward:", words.join(', '));
-
-// Display backward
-console.log("Backward:", words.slice().reverse().join(', '));
