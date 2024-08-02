70be8774c9c3ff94f302fa7d14e0504b5f25d7e9
kelvintechsolutions <kevohmutugi51@gmail.com>
2024-08-02 10:09:27 +0300

    store forward and backward

---
+++ store back and front.js
@@ -0,0 +1,7 @@
+const words = ["apple", "banana", "cherry", "date", "elderberry"];
+
+// Display forward
+console.log("Forward:", words.join(', '));
+
+// Display backward
+console.log("Backward:", words.slice().reverse().join(', '));
