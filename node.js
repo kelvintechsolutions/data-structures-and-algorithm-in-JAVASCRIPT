a0e53415638ee194c432b1eb0ece0a5f96f05f93
kelvintechsolutions <kevohmutugi51@gmail.com>
2024-08-28 09:30:02 +0300

    updates

--- node.js
+++ node.js
@@ -1,4 +1,14 @@
-class Node {
+/* According to legend, the first-century Jewish historian Flavius Josephus was about
+to be captured along with a band of 40 compatriots by Roman soldiers during the
+Jewish-Roman War. The Jewish soldiers decided that they preferred suicide to being
+captured and devised a plan for their demise. They were to form a circle and kill
+every third soldier until they were all dead. Josephus and one other decided they
+wanted no part of this and quickly calculated where they needed to place themselves
+so they would be the last survivors. Write a program that allows you to place n
+people in a circle and specify that every mth person will be killed. The program
+should determine the number of the last two people left in the circle. Use a circularly
+linked list to solve the problem. */
+class Nodew {
     constructor(value) {
         this.value = value;
         this.next = null;
