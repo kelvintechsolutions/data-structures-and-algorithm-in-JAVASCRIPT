1e350e0fb5f1e7906d42d5661b155bb381fd3d0a
kelvintechsolutions <kevohmutugi51@gmail.com>
2024-08-28 10:39:16 +0300

    updates

--- hash table linear probing.js
+++ hash table linear probing.js
@@ -1,3 +1,7 @@
+/* Use linear probing to create a simple dictionary to store the definitions of words.
+Your program should have two parts. The first part reads a text file that contains a
+list of words and definitions and stores them in a hash table. The second part of the
+program allows a user to enter a word and see the definition of that word. */
 class HashTable {
     constructor(size) {
         this.size = size;
