9020684a345c0121ca5bd8368a9fdb36dde518cc
kelvintechsolutions <kevohmutugi51@gmail.com>
2024-09-28 14:41:03 +0300

    updates

---
+++ LEETCODE DATA STRUCTURES/Trapping Rain Water.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let left = 0, right = height.length - 1;
+    let left_max = 0, right_max = 0;
+    let trapped_water = 0;
+    
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= left_max) {
+                left_max = height[left];
+            } else {
+                trapped_water += left_max - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= right_max) {
+                right_max = height[right];
+            } else {
+                trapped_water += right_max - height[right];
+            }
+            right--;
+        }
+    }
+    
+    return trapped_water;
+};
